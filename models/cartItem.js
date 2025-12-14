f71c92107ec4abacf5a7f2bf73386e9190d79b6c
Nowellah <abridgetnowellah@gmail.com>
2025-12-14 20:39:13 +0300

    modify cartItm model

--- models/cartItem.js
+++ models/cartItem.js
@@ -1,18 +1,30 @@
 import mongoose from 'mongoose';
 
-const cartItemSchema = new mongoose.Schema({
+const cartItemSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
     productId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'product',
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
     },
     quantity: {
-        type: Number,
-        required:  true,
-        default: 1
-    }
-});
+      type: Number,
+      required: true,
+      default: 1,
+      min: 1,
+    },
+    priceAtAddition: {
+      type: Number,
+      required: false, // optional snapshot of product price when added
+    },
+  },
+  { timestamps: true }
+);
 
-const CartItem = mongoose.model("CartItem", cartItemSchema);
+const CartItem = mongoose.model('CartItem', cartItemSchema);
 export default CartItem;
-
