1dd9b73e5d1e88255951babf5caec6bbc7cfb5f3
Nowellah <abridgetnowellah@gmail.com>
2025-12-15 18:56:28 +0300

    add error handler middleware

---
+++ middleware/errorHandler.js
@@ -0,0 +1,12 @@
+export default function errorHandler(err, req, res, next) {
+  console.error('Error:', err);
+
+  // Default values
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+
+  res.status(statusCode).json({
+    success: false,
+    message: err.message || 'Server Error',
+    stack: process.env.NODE_ENV === 'development' ? err.stack : undefined,
+  });
+}
